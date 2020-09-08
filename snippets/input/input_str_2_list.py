e6f73388fde32f34bddf4c3b0396c270f06d80a0
Akira K <kira@kirameister.net>
2020-09-09 01:38:49 +0200

    add input_str_2_list.py

---
+++ snippets/input/input_str_2_list.py
@@ -0,0 +1,13 @@
+# 単純に入力を整数として取り込む
+A = int(input())
+
+# 複数の入力の値を整数として異なる変数に入れる
+[A, B] = map(int, list(input().split(' ')))
+
+# 最初の値は変数に、それ以降の値をリストにする
+A, *B = map(int, list(input().split(' ')))
+# または...
+[A, *B] = map(int, list(input().split(' ')))
+
+
+
